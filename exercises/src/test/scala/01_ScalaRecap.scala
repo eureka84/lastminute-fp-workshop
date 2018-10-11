ee8cecfd485c599caaa7cc5c5000dd931378a031
asciarra <angelo.sciarra@lastminute.com>
2018-10-11 19:45:25 +0200

    ScalaRecap

--- exercises/src/test/scala/01_ScalaRecap.scala
+++ exercises/src/test/scala/01_ScalaRecap.scala
@@ -1,5 +1,6 @@
 package exercises
 
+import exercises.answers.ScalaRecap.Banana
 import minitest._
 
 /*
@@ -26,84 +27,101 @@ object ScalaRecap extends SimpleTestSuite {
    */
 
   test("define case class") {
-    ignore("define a case class w/ two fields: name and age")
-    // val result = Person("foo", 56)
-    // assertEquals(result, Person("foo", 56))
+    val result = Person("foo", 56)
+    assertEquals(result, Person("foo", 56))
   }
 
   test("define the case class's companion object") {
-    ignore("define a companion object w/ a custom factory method")
-    // val result = Person.create("foo, 56")
-    // assertEquals(result, Person("foo", 56))
+    val result = Person.create("foo, 56")
+    assertEquals(result, Person("foo", 56))
   }
 
   test("case class apply") {
-    ignore("add an apply function on Person class")
-    // val result = Person("foo", 56)("Ciao,")
-    // assertEquals(result, "Ciao, mi chiamo foo!")
-  }
-
-  test("companion object apply") {
-    ignore("add an apply function on Person object")
-    // val result = Person("foo, 56")("Ciao,")
-    // assertEquals(result, "Ciao, mi chiamo foo!")
+    val result = Person("foo", 56)("Ciao,")
+    assertEquals(result, "Ciao, mi chiamo foo!")
   }
 
   test("case class update") {
-    ignore("add 100 years to the person")
-    // val p      = Person("foo", 56)
-    // val result = p.copy(age = p.age + 100)
-    // assertEquals(result.age, 156)
+    val p      = Person("foo", 56)
+    val result = p.copy(age = p.age + 100)
+    assertEquals(result.age, 156)
   }
 
   test("trait as interface") {
-    ignore("add a Fruit trait w/ two subclass Apple and Banana")
-    // assert(Apple().isInstanceOf[Fruit])
-    // assert(Banana().isInstanceOf[Fruit])
+    assert(Apple().isInstanceOf[Fruit])
+    assert(Banana().isInstanceOf[Fruit])
 
-    ignore("add empty stringify function to Fruit and implement it in Apple and Banana")
-    // assertEquals(Apple().stringify, "an apple")
-    // assertEquals(Banana().stringify, "a banana")
+    assertEquals(Apple().stringify, "an apple")
+    assertEquals(Banana().stringify, "a banana")
   }
 
   test("trait as mixin") {
-    ignore("add function w/ default implementation to Fruit trait")
-    // assertEquals(Apple().eatenBy("foo"), "foo ate an apple")
-    // assertEquals(Banana().eatenBy("bar"), "bar ate a banana")
+    assertEquals(Apple().eatenBy("foo"), "foo ate an apple")
+    assertEquals(Banana().eatenBy("bar"), "bar ate a banana")
   }
 
   test("type parameter") {
-    ignore("add generic function to Person class")
-    // val p      = Person("foo", 56)
-    // val result = p.eat(Apple())
-    // assertEquals(result, "foo ate an apple")
+     val p      = Person("foo", 56)
+     val result = p.eat(Apple())
+     assertEquals(result, "foo ate an apple")
   }
 
   test("implicit parameter") {
-    ignore("add a function w/ an implicit parameter to the Person class")
-    // implicit val years = 30
-    // val p              = Person("foo", 56)
-    // val result         = p.makeYounger
-    // assertEquals(result.age, 26)
+//    ignore("add a function w/ an implicit parameter to the Person class")
+     implicit val years = 30
+     val p              = Person("foo", 56)
+     val result         = p.makeYounger
+     assertEquals(result.age, 26)
   }
 
   test("extension class") {
-    ignore("add a function to Person via implicit class extension")
-    // val p      = Person("foo", 56)
-    // val result = p.toMap
-    // assertEquals(result, Map("name" -> "foo", "age" -> "56"))
+//    ignore("add a function to Person via implicit class extension")
+     val p      = Person("foo", 56)
+     val result = p.toMap
+     assertEquals(result, Map("name" -> "foo", "age" -> "56"))
+  }
+
+  def isFake(person: Person): Boolean = person match {
+    case Person("bar", _) => true
+    case Person("foo", _) => true
+    case Person(_, age) if age < 0 => true
+    case _ => false
+
   }
 
   test("pattern match") {
-    ignore("add a function to Person object that...")
-    ignore("...return true when name is foo")
-    ignore("...return true when name is bar")
-    ignore("...return true when age is negative")
-    ignore("...otherwise return false")
-    // import Person._
-    // assert(isFake(Person("foo", 10)))
-    // assert(isFake(Person("bar", 10)))
-    // assert(isFake(Person("matte", -10)))
-    // assert(!isFake(Person("matte", 10)))
+     import Person._
+     assert(isFake(Person("foo", 10)))
+     assert(isFake(Person("bar", 10)))
+     assert(isFake(Person("matte", -10)))
+     assert(!isFake(Person("matte", 10)))
   }
 }
+case class Person(name: String, age: Int) {
+  def makeYounger(implicit years: Int): Person = Person(name, age - years)
+
+
+  def eat[T <: Fruit](fruit: T): String = fruit.eatenBy(name)
+
+  def apply(message: String): String = s"Ciao, mi chiamo $name!"
+}
+object Person {
+  def create(strValue: String): Person = {
+    val parts = strValue.split(",")
+    Person(parts(0), parts(1).trim.toInt)
+  }
+  implicit class RichPerson(p: Person) extends Person(p.name, p.age){
+    def toMap: Map[String, String] =
+      Map("name" -> p.name, "age" -> p.age.toString)
+  }
+}
+
+sealed trait Fruit {
+  def stringify: String = this match {
+    case Apple()  => "an apple"
+    case Banana() => "a banana"
+  }
+  def eatenBy(eater: String): String = s"$eater ate $stringify"
+}
+case class Apple()  extends Fruit
+case class Banana() extends Fruit
