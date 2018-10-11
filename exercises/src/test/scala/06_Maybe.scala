e6f3ee505b08ab188f0ad67c0e9b86467b28d027
asciarra <angelo.sciarra@lastminute.com>
2018-10-11 20:47:43 +0200

    call me maybe

--- exercises/src/test/scala/06_Maybe.scala
+++ exercises/src/test/scala/06_Maybe.scala
@@ -17,20 +17,24 @@ object MaybeTests extends SimpleTestSuite {
    * TODO: remove all nulls
    */
 
+  sealed trait Effect[+T]
+  case class Success[T](v: T) extends Effect[T]
+  case object Failure extends Effect[Nothing]
+
   case class Qty(value: Int)
 
-  def toQty(value: String): Qty =
-    if (value.matches("^[0-9]+$")) Qty(value.toInt)
-    else null
+  def toQty(value: String): Effect[Qty] =
+    if (value.matches("^[0-9]+$")) Success(Qty(value.toInt))
+    else Failure
 
   test("valid qty") {
-    assertEquals(toQty("100"), Qty(100))
+    assertEquals(toQty("100"), Success(Qty(100)))
   }
 
   test("invalid qty") {
-    assertEquals(toQty("asd"), null)
-    assertEquals(toQty("1 0 0"), null)
-    assertEquals(toQty(""), null)
-    assertEquals(toQty("-10"), null)
+    assertEquals(toQty("asd"), Failure)
+    assertEquals(toQty("1 0 0"), Failure)
+    assertEquals(toQty(""), Failure)
+    assertEquals(toQty("-10"), Failure)
   }
 }
