0a0d5ab1b33d232a8b718d8e176cd49a509ea175
asciarra <angelo.sciarra@lastminute.com>
2018-10-12 17:56:37 +0200

    13 done

--- exercises/src/test/scala/13_TypeclassHK.scala
+++ exercises/src/test/scala/13_TypeclassHK.scala
@@ -40,25 +40,45 @@ object TypeclassHKTests extends SimpleTestSuite {
    *       Implements MRUList.add.
    */
 
-  trait Stack[F[_]] {}
+  trait Stack[F[_]] {
+    def pop[A](items: F[A]): (Option[A], F[A])
+    def push[A](value: A, items: F[A]): F[A]
+  }
 
-  implicit val listStack = new Stack[List] {}
+  implicit val listStack = new Stack[List] {
+    override def push[A](value: A, items: List[A]): List[A]   = value :: items
+    override def pop[A](items: List[A]): (Option[A], List[A]) =
+      (items.take(1).headOption, items.tail)
+  }
 
   object Stack {
-    def apply[F[_]](implicit x: Stack[F]) = x
+    def apply[F[_]](implicit x: Stack[F]): Stack[F] = x
   }
 
   object MRUList {
     def add[F[_]: Stack, A](value: A, items: F[A]): F[A] =
-      ???
+      Stack[F].push(value, items)
+
+    def take[F[_]: Stack, A](items: F[A]): (Option[A], F[A]) =
+      Stack[F].pop(items)
   }
 
   test("add an element to the MRU list") {
     import MRUList._
 
-    ignore("implement missing functions")
-    val items  = List("first")
-    val result = add("second", items)
-    assertEquals(result, List("second", "first"))
+//    ignore("implement missing functions")
+    val items  = List(1)
+    val result = add(2, items)
+    assertEquals(result, List(2, 1))
+  }
+
+  test("remove an element to the MRU list") {
+    import MRUList._
+
+    //    ignore("implement missing functions")
+    val items  = List(2, 1)
+    val (optionalValue, newItems) = take(items)
+    assertEquals(optionalValue, Some(2))
+    assertEquals(newItems, List(1))
   }
 }
