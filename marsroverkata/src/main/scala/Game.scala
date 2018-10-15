2a7ea1d9b6a42b18b554505acfa1e7a8991eb15c
eureka84 <angelosciarra@ymail.com>
2018-10-15 14:41:47 +0200

    refactored and handled obstacles

--- marsroverkata/src/main/scala/Game.scala
+++ marsroverkata/src/main/scala/Game.scala
@@ -1,7 +1,6 @@
 package marsroverkata
 
 import cats.effect.IO
-
 import marsroverkata.Data._
 import marsroverkata.GameInteractions._
 import marsroverkata.GamePlay._
@@ -14,9 +13,9 @@ class Game {
   def runIO(): IO[Unit] =
     welcome()
       .flatMap(_ => readPlanet())
-      .flatMap(p => readObstacles().map(os => p.copy(obstacles = os)))
-      .flatMap(p => readPosition().map(pos => Rover(pos, N, p)))
-      .flatMap(r => readCommands().map(cs => handleCommands(r, cs)))
-      .flatMap(r => display(r))
+      .flatMap(planet => readObstacles().map(os => planet.copy(obstacles = os)))
+      .flatMap(planet => readPosition().map(pos => Rover(pos, N, planet)))
+      .flatMap(rover => readCommands().map(cs => handleCommands(rover, cs)))
+      .flatMap(result => display(result))
 
 }
